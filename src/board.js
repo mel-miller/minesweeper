e6a0340db3e5d71561908d982c15073a502c3e75
M Miller <miller.2676@osu.edu>
2018-07-16 15:04:15 -0700

    parse Board and Game classes to their own files

--- src/board.js
+++ src/board.js
@@ -1,25 +1,4 @@
-class Game {
-  constructor(numberOfRows, numberOfColumns, numberOfBombs) {
-    this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
-  }
-
-  playMove(rowIndex, columnIndex) {
-    this._board.flipTile(rowIndex, columnIndex);
-    if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {
-      console.log('Bomb! Game over. Here is the final board:');
-      this._board.print();
-    } else if (!this._board.hasSafeTiles()) {
-      console.log("You won! Here is your game board:");
-      this._board.print();
-    } else {
-      console.log('Current Board:');
-      this._board.print();
-    }
-  }
-} // end Game class
-
-
-class Board {
+export class Board {
   constructor(numberOfRows, numberOfColumns, numberOfBombs) {
     this._numberOfBombs = numberOfBombs;
     this._numberOfEmptySpaces = numberOfRows * numberOfColumns;
@@ -118,7 +97,4 @@ class Board {
     }
     return board;
   }
-} // end Board class
-
-const g = new Game(3, 3, 3);
-g.playMove(0, 0);
+}
---
+++ src/game.js
@@ -0,0 +1,21 @@
+import {Board} from './board';
+
+class Game {
+  constructor(numberOfRows, numberOfColumns, numberOfBombs) {
+    this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
+  }
+
+  playMove(rowIndex, columnIndex) {
+    this._board.flipTile(rowIndex, columnIndex);
+    if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {
+      console.log('Bomb! Game over. Here is the final board:');
+      this._board.print();
+    } else if (!this._board.hasSafeTiles()) {
+      console.log("You won! Here is your game board:");
+      this._board.print();
+    } else {
+      console.log('Current Board:');
+      this._board.print();
+    }
+  }
+}
